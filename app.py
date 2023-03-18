d719701545bdd714408a11fb46a8d23f78cc8440
lennonay <lennon.auyeung@me.com>
2023-03-17 23:24:13 -0700

    new_app and data

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import dash
 from dash import html, dcc, Input, Output
-import altair as alt
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
-
-alt.data_transformers.enable('data_server')
-
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
@@ -19,55 +15,6 @@ covid_data = covid_data[(covid_data['continent']).notnull()]
 location = list(covid_data.location.unique())
 location.append('World')
 
-def plot_cases(country, median_age, human_index):
-    if (country != 'World'):
-        toal_case_plot = covid_data[covid_data['location'] == country]
-    else: toal_case_plot = covid_data
-    age_covid_data = toal_case_plot[(toal_case_plot['median_age'] > median_age[0] ) & (toal_case_plot['median_age'] < median_age[1])]
-    df = age_covid_data[(age_covid_data['human_development_index'] > human_index[0] ) & (age_covid_data['human_development_index'] < human_index[1])]
-    df = pd.DataFrame(df.groupby('date')['total_cases'].sum().reset_index())
-    chart = alt.Chart(df, title = 'Total cases over time').mark_line().encode(
-        x='date',
-        y='total_cases',
-        tooltip = alt.Tooltip('total_cases',format = ',')
-    ).properties(
-    width=800,
-    height=280).interactive()
-    fig = px.line(
-    df, x="date", y="total_cases",labels = {'total_cases': 'Total Cases', 'date': 'Date'},title = 'Total cases over time')
-    return fig
-    #return chart.to_html()
-
-def plot_vaccinations(country, median_age, human_index):
-    if (country != 'World'):
-        toal_case_plot = covid_data[covid_data['location'] == country]
-    else: toal_case_plot = covid_data
-    age_covid_data = toal_case_plot[(toal_case_plot['median_age'] > median_age[0] ) & (toal_case_plot['median_age'] < median_age[1])]
-    df = age_covid_data[(age_covid_data['human_development_index'] > human_index[0] ) & (age_covid_data['human_development_index'] < human_index[1])]
-    df = pd.DataFrame(df.groupby('date')['new_vaccinations_smoothed'].sum().reset_index())
-    chart = alt.Chart(df, title = 'New vaccinations smoothed over time').mark_line().encode(
-        x='date',
-        y='new_vaccinations_smoothed',
-        tooltip = alt.Tooltip('new_vaccinations_smoothed',format = ',')
-    ).properties(
-    width=800,
-    height=280).interactive()
-    fig = px.line(
-    df, x="date", y="new_vaccinations_smoothed",labels = {'new_vaccinations_smoothed': 'New vaccinations smoothed', 'date': 'Date'},title = 'New vaccinations smoothed over time')
-    return fig
-    #return chart.to_html()
-
-def plot_stringency(country, median_age, human_index):
-    if (country != 'World'):
-        toal_case_plot = covid_data[covid_data['location'] == country]
-    else: toal_case_plot = covid_data
-    age_covid_data = toal_case_plot[(toal_case_plot['median_age'] > median_age[0] ) & (toal_case_plot['median_age'] < median_age[1])]
-    df = age_covid_data[(age_covid_data['human_development_index'] > human_index[0] ) & (age_covid_data['human_development_index'] < human_index[1])]
-    df = pd.DataFrame(df.groupby('location')['stringency_index'].mean().reset_index())
-    fig = px.choropleth(df, locations = 'location',locationmode= 'country names',
-              color = 'stringency_index', title = 'Mean stringency index of country')
-    #fig.update_layout(width=1000, height=600)
-    return fig
     
 app.layout = dbc.Container([
     html.H1('COVID Dashboard',style={
@@ -94,32 +41,49 @@ app.layout = dbc.Container([
                 'padding': 15,
                 'border-radius': 3}),
     dbc.Col([
-        dcc.Graph(
-            id = 'stringency',
-            figure = plot_stringency('World',[15,35],[0.3,1])
-        ),
-        dcc.Graph(
-            id = 'total_cases',
-            figure=plot_cases('World',[15,35],[0.3,1]),),
-            #style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Graph(
-            id = 'total_vaccinations',
-            figure=plot_vaccinations('World',[15,35],[0.3,1]),)
-            #style={'border-width': '0', 'width': '100%', 'height': '400px'})
-    ])])])
+        dcc.Graph(id = 'pie_chart'),
+        dcc.Graph(id = 'world_map')
+        ]),
+    dbc.Col([
+        dcc.Graph(id = 'total_cases'),
+        dcc.Graph(id = 'new_vac')
+    ],md=4)
+    ])])
 
 # Set up callbacks/backend
 @app.callback(
+    Output('pie_chart', 'figure'),
+    Output('world_map', 'figure'),
     Output('total_cases', 'figure'),
-    Output('total_vaccinations', 'figure'),
-    Output('stringency', 'figure'),
+    Output('new_vac', 'figure'),
     Input('xcol', 'value'),
     Input('xslider', 'value'),
     Input('hslider','value')
 )
+def generate_chart(xcol,xslider,hslider):
+    if (xcol != 'World'):
+        int_plot = covid_data[covid_data['location'] == xcol]
+    else: int_plot = covid_data
+    
+    age_covid_data = int_plot[(int_plot['median_age'] > xslider[0] ) & (int_plot['median_age'] < xslider[1])]
+    df = age_covid_data[(age_covid_data['human_development_index'] > hslider[0] ) & (age_covid_data['human_development_index'] < hslider[1])]
+    
+    pie_chart = px.pie(df, values='total_cases', names='continent', title='Total Cases of Different Continent')
+    
+    map_df = pd.DataFrame(df.groupby('location')['stringency_index'].mean().reset_index())
+    world_map = px.choropleth(map_df, locations = 'location',locationmode= 'country names',
+              color = 'stringency_index', title = 'Mean stringency index of country')
+    
+    new_vac_df = pd.DataFrame(df.groupby('date')['new_vaccinations_smoothed'].sum().reset_index())
+    new_vac = px.line(new_vac_df, x="date", y="new_vaccinations_smoothed",labels = {'new_vaccinations_smoothed': 'New vaccinations smoothed', 'date': 'Date'},title = 'New vaccinations smoothed over time')
+    
+    total_cases_df = pd.DataFrame(df.groupby('date')['new_cases'].sum().reset_index())
+    total_cases_df = total_cases_df.set_index('date')
+    total_cases_df = total_cases_df.resample("M").sum()
+    total_cases = px.bar(total_cases_df, x=total_cases_df.index, y="new_cases",labels = {'new_cases': 'New Cases', 'date': 'Date'},title = 'News cases by month')
 
-def update_output(xcol,xslider,hslider):
-    return plot_cases(xcol,xslider,hslider), plot_vaccinations(xcol,xslider,hslider), plot_stringency(xcol,xslider,hslider)
+
+    return pie_chart,world_map,total_cases, new_vac
 
 if __name__ == '__main__':
     app.run_server(debug=True)
